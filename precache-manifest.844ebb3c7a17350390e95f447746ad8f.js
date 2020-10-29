91f6abc9a62bcd3adcd27fa4cd350d410176de93
dannyhoang <hnthai@humaxdigital.com>
2020-10-29 20:11:41 +0700

    update

---
+++ precache-manifest.844ebb3c7a17350390e95f447746ad8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5fcfd2f0deab9511f8ac9e46c6f9c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad338c2de28e9ee44cd9",
+    "url": "./static/css/main.a699ed8c.chunk.css"
+  },
+  {
+    "revision": "d78519df44ec2c64bd4e",
+    "url": "./static/js/2.ec2d356b.chunk.js"
+  },
+  {
+    "revision": "ad338c2de28e9ee44cd9",
+    "url": "./static/js/main.53543254.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
